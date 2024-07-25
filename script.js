20c1ea73011d5a1d390844f9488b17ca762bd01f
Erin <erinrainabo35@gmail.com>
2024-07-25 18:59:30 +0400

    Make responsive design changes

--- script.js
+++ script.js
@@ -1,18 +1,43 @@
 const sidebar = document.querySelector('.js-sidebar');
 const menuButton = document.querySelector('.js-menu-button');
 const closeButton = document.querySelector('.js-close-button')
+const userGreetingElement = document.querySelector('.js-user-greeting')
 
 const openSidebar = () => {
-  menuButton.addEventListener('click', () => {
-    sidebar.style.display = 'block'
-  })
+  sidebar.style.display = 'block'
 }
 
 const closeSidebar = () => {
-  closeButton.addEventListener('click', () => {
-    sidebar.style.display = 'none'
+  sidebar.style.display = 'none'
+}
+
+if (window.innerWidth < 1130) {
+  closeSidebar()
+
+  menuButton.addEventListener('click', () => {
+    openSidebar()
   })
 }
 
-openSidebar()
-closeSidebar()
+window.addEventListener('resize', () => {
+  if (window.innerWidth < 1130) {
+    closeSidebar()
+
+    menuButton.addEventListener('click', () => {
+      openSidebar()
+    })
+
+    closeButton.addEventListener('click', () => {
+      closeSidebar()
+    })
+
+  } else if (window.innerWidth > 1130) {
+    openSidebar()
+  } 
+  
+})
+
+if (window.innerWidth <= 700) {
+  userGreetingElement.innerHTML = 'Morgan Oakley'
+  console.log(userGreetingElement)
+}
